66bd7c9265f4a27687b10d9746b8e5f88b4a825e
Emmanuel <twentekghana@gmail.com>
2018-12-27 09:38:19 +0800

    net income has been created

--- js/app.js
+++ js/app.js
@@ -1,22 +1,29 @@
 const myAccount = {
     userName: "Emmanuel",
     income: 0,
-    expenses: 0
+    expenses: 0,
+    netIncome: 0,
+    incomeSummary: function () {
+        let final = this.income - this.expenses;
+        document.querySelector("#net-income").innerHTML = final;
+    }
 }
 
 
-
-
+//Add Expenses function
 let addExpenses = function (account) {
     let expensesAmount = parseFloat(document.querySelector("#expenses-amount").value)
-    account.expenses = account.expenses + expensesAmount;
 
-    account.expenses = account.expenses + expensesAmount;
-    document.querySelector("#totalExpenses").innerHTML = account.expenses;
+    if (isNaN(expensesAmount)) {
+        alert("Please Enter a number")
+    } else {
+        account.expenses = account.expenses + expensesAmount;
+        document.querySelector("#totalExpenses").innerHTML = account.expenses;
+    }
 
 }
 
-
+//Add Income function
 let addIncome = function (account) {
     let incomeAmount = parseFloat(document.querySelector("#income-amount").value)
 
@@ -26,23 +33,23 @@ let addIncome = function (account) {
         account.income = account.income + incomeAmount;
         document.querySelector("#totalIncome").innerHTML = account.income;
     }
-
 }
-
-
 
 //Event Listeners
 
+//Add Income evetn listener
 document.querySelector("#addIncome-btn").addEventListener("click", function (e) {
     e.preventDefault()
     addIncome(myAccount)
+    myAccount.incomeSummary()
+
 })
 
 
-
-
+//Add expenses event listener
 document.querySelector("#addExpenses").addEventListener("click", function (e) {
     e.preventDefault()
     addExpenses(myAccount)
+    myAccount.incomeSummary()
 
 })
